38492d3b8c0016616bc1334df834dff69e4b6624
iqbalgading10 <iqbalgading123@gmail.com>
2022-07-04 17:57:47 +0700

    berhasil

--- src/configs/database.js
+++ src/configs/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     multipleStatements  : true,
     host                : "sql6.freemysqlhosting.net",
-    user                : "sql6498920",
-    password            : "atwEmgTqwj",
-    database            : "sql6498920"
+    user                : "sql6503925",
+    password            : "NgfZavfaID",
+    database            : "sql6503925"
 };
